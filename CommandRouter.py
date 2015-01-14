32979ae1420c671837f9de3f94b141255af69a9f
argunner <argunner@users.noreply.github.com>
2015-01-14 19:28:25 +0530

    Update CommandRouter.py
    Added Some New Patches with appropriate comments.I needed similar code for my project so i thought why not Add these features to Master Repo.

--- CommandRouter.py
+++ CommandRouter.py
@@ -9,9 +9,49 @@ class commandRouter :
 
 		self.handlers[handlerName] = handler
 
-	def handle(self, handlerName, what) :
+	
+	def build_log(self, handlerName , msg):
+		
+		#log file to keep track for failed request for later analysis
+		f = open("log_handled.txt","a") #opens file with name of "log_handled.txt"
+		#f.write(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+		#f.write(time.time())
+		f.write(handlerName)
+		f.write("\n")
+		f.write(msg)
+		f.write("\n\n\n")
 
-		if handlerName in self.handlers :
-			self.handlers[handlerName].handle(what)
-		else :
-			raise Exception("No handler present")
+	def handle(self, handlerName, what, multiple_instances = "FALSE", prepare_log = "FALSE") :
+
+		# multiple handle instances can be associated to same handlerName they should be processed seperately
+		# giving priority to each instance
+		# each instance holds different memory space
+		# however multiple instace occurance of handles will be treated as one by default
+
+		if(multiple_instances == "TRUE"):
+			
+			if handlerName in self.handlers:
+
+				i = 0
+			
+				for handlerName in self.handlers :
+					self.handlers[handlerName].handle(what)
+					#uncomment/comment line below to hide/show instance memory address if multiple
+					print(self.handlers[handlerName])
+					i = i+1
+					if(i > 1):
+						print "More than 1 Instances Were Found To Be Associated"
+			else :
+				print "No handler present"
+				if(prepare_log == "TRUE"):
+					self.build_log(handlerName,"No handler present")
+				
+		else:
+			if handlerName in self.handlers :
+				self.handlers[handlerName].handle(what)
+			else:
+				print "No handler present"
+				if(prepare_log == "TRUE"): 
+                                        self.build_log(handlerName,"No handler present")
+
+			
